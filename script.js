4511b677ef5330da08fcaf2fd9567b041ac9fe4f
tuptop <pavellemba@gmail.com>
2023-04-10 04:43:25 +0300

    Табы готовы

---
+++ script.js
@@ -0,0 +1,236 @@
+function changeItem12() {
+    document.getElementById('LL1').style.display = 'none';
+    document.getElementById('LL2').style.display = 'block';
+  }
+
+  function rechangeItem12() {
+    document.getElementById('LL1').style.display = 'block';
+    document.getElementById('LL2').style.display = 'none';
+  }
+
+
+  function changeItem13() {
+    document.getElementById('LL1').style.display = 'none';
+    document.getElementById('LL3').style.display = 'block';
+  }
+  
+  function rechangeItem13() {
+    document.getElementById('LL1').style.display = 'block';
+    document.getElementById('LL3').style.display = 'none';
+  }
+
+
+  function changeItem14() {
+    document.getElementById('LL1').style.display = 'none';
+    document.getElementById('LL4').style.display = 'block';
+  }
+  
+  function rechangeItem14() {
+    document.getElementById('LL1').style.display = 'block';
+    document.getElementById('LL4').style.display = 'none';
+  }
+
+
+  function changeItem15() {
+    document.getElementById('LL1').style.display = 'none';
+    document.getElementById('LL5').style.display = 'block';
+  }
+  
+  function rechangeItem15() {
+    document.getElementById('LL1').style.display = 'block';
+    document.getElementById('LL5').style.display = 'none';
+  }
+
+
+  function changeItem16() {
+    document.getElementById('LL1').style.display = 'none';
+    document.getElementById('LL6').style.display = 'block';
+  }
+  
+  function rechangeItem16() {
+    document.getElementById('LL1').style.display = 'block';
+    document.getElementById('LL6').style.display = 'none';
+  }
+
+
+  function changeItem17() {
+    document.getElementById('LL1').style.display = 'none';
+    document.getElementById('LL7').style.display = 'block';
+  }
+  
+  function rechangeItem17() {
+    document.getElementById('LL1').style.display = 'block';
+    document.getElementById('LL7').style.display = 'none';
+  }
+
+
+
+
+
+
+  function changeItem22() {
+    document.getElementById('LL21').style.display = 'none';
+    document.getElementById('LL22').style.display = 'block';
+  }
+  
+  function rechangeItem22() {
+    document.getElementById('LL21').style.display = 'block';
+    document.getElementById('LL22').style.display = 'none';
+  }
+
+
+  function changeItem23() {
+    document.getElementById('LL21').style.display = 'none';
+    document.getElementById('LL23').style.display = 'block';
+  }
+  
+  function rechangeItem23() {
+    document.getElementById('LL21').style.display = 'block';
+    document.getElementById('LL23').style.display = 'none';
+  }
+
+
+
+
+
+
+  
+  function changeItem32() {
+    document.getElementById('LL31').style.display = 'none';
+    document.getElementById('LL32').style.display = 'block';
+  }
+  
+  function rechangeItem32() {
+    document.getElementById('LL31').style.display = 'block';
+    document.getElementById('LL32').style.display = 'none';
+  }
+
+
+  function changeItem33() {
+    document.getElementById('LL31').style.display = 'none';
+    document.getElementById('LL33').style.display = 'block';
+  }
+  
+  function rechangeItem33() {
+    document.getElementById('LL31').style.display = 'block';
+    document.getElementById('LL33').style.display = 'none';
+  }
+
+
+  function changeItem34() {
+    document.getElementById('LL31').style.display = 'none';
+    document.getElementById('LL34').style.display = 'block';
+  }
+  
+  function rechangeItem34() {
+    document.getElementById('LL31').style.display = 'block';
+    document.getElementById('LL34').style.display = 'none';
+  }
+
+
+
+
+
+
+  function changeItem52() {
+    document.getElementById('LL51').style.display = 'none';
+    document.getElementById('LL52').style.display = 'block';
+  }
+  
+  function rechangeItem52() {
+    document.getElementById('LL51').style.display = 'block';
+    document.getElementById('LL52').style.display = 'none';
+  }
+
+
+  function changeItem53() {
+    document.getElementById('LL51').style.display = 'none';
+    document.getElementById('LL53').style.display = 'block';
+  }
+  
+  function rechangeItem53() {
+    document.getElementById('LL51').style.display = 'block';
+    document.getElementById('LL53').style.display = 'none';
+  }
+
+
+  function changeItem54() {
+    document.getElementById('LL51').style.display = 'none';
+    document.getElementById('LL54').style.display = 'block';
+  }
+  
+  function rechangeItem54() {
+    document.getElementById('LL51').style.display = 'block';
+    document.getElementById('LL54').style.display = 'none';
+  }
+  
+
+  function changeItem55() {
+    document.getElementById('LL51').style.display = 'none';
+    document.getElementById('LL55').style.display = 'block';
+  }
+  
+  function rechangeItem55() {
+    document.getElementById('LL51').style.display = 'block';
+    document.getElementById('LL55').style.display = 'none';
+  }
+  
+
+
+
+
+
+  function changeItem62() {
+    document.getElementById('LL61').style.display = 'none';
+    document.getElementById('LL62').style.display = 'block';
+  }
+  
+  function rechangeItem62() {
+    document.getElementById('LL61').style.display = 'block';
+    document.getElementById('LL62').style.display = 'none';
+  }
+
+
+  function changeItem63() {
+    document.getElementById('LL61').style.display = 'none';
+    document.getElementById('LL63').style.display = 'block';
+  }
+  
+  function rechangeItem63() {
+    document.getElementById('LL61').style.display = 'block';
+    document.getElementById('LL63').style.display = 'none';
+  }
+
+
+  function changeItem64() {
+    document.getElementById('LL61').style.display = 'none';
+    document.getElementById('LL64').style.display = 'block';
+  }
+  
+  function rechangeItem64() {
+    document.getElementById('LL61').style.display = 'block';
+    document.getElementById('LL64').style.display = 'none';
+  }
+  
+
+  function changeItem65() {
+    document.getElementById('LL61').style.display = 'none';
+    document.getElementById('LL65').style.display = 'block';
+  }
+  
+  function rechangeItem65() {
+    document.getElementById('LL61').style.display = 'block';
+    document.getElementById('LL65').style.display = 'none';
+  }
+  
+
+  function changeItem66() {
+    document.getElementById('LL61').style.display = 'none';
+    document.getElementById('LL66').style.display = 'block';
+  }
+  
+  function rechangeItem66() {
+    document.getElementById('LL51').style.display = 'block';
+    document.getElementById('LL66').style.display = 'none';
+  }
+  
