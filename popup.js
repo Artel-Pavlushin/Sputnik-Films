9ec262c2b8fa369217ad7a4573c6e2eea1cd5ef0
tuptop <pavellemba@gmail.com>
2023-04-19 02:58:54 +0300

    правки «о нас»

---
+++ popup.js
@@ -0,0 +1,7 @@
+$('.image-popup-zoom').magnificPopup({
+    type: 'image',
+    zoom: {
+        enabled: true,
+        duration: 300 // продолжительность анимации. Не меняйте данный параметр также и в CSS
+    }
+    });
