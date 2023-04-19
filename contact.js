22fd9a2edd2d2547641502c6a1671294cd9116ee
tuptop <pavellemba@gmail.com>
2023-04-20 00:29:34 +0300

    правки

--- contact.js
+++ contact.js
@@ -7,6 +7,11 @@ function changeText1(text) {
     display.innerHTML = "";
     display.innerHTML = text;
 }
+function changeText2(text) {
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
 function changeback1(text) {
     var display = document.getElementById('text-display');
     display.innerHTML = "";
@@ -14,6 +19,11 @@ function changeback1(text) {
 }
 
 function changeText(text) {
+    var display = document.getElementById('text-display1');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+function changeText3(text) {
     var display = document.getElementById('text-display1');
     display.innerHTML = "";
     display.innerHTML = text;
