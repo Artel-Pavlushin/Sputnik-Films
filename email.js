14190f82721f856195e73284cc33844f28fb7f57
tuptop <pavellemba@gmail.com>
2023-04-20 10:25:05 +0300

    Фон

--- email.js
+++ email.js
@@ -1,9 +1,11 @@
-let text = document.getElementById('myText').innerHTML;
-const copyContent = async () => {
-  try {
-    await navigator.clipboard.writeText(text);
-    console.log('Content copied to clipboard');
-  } catch (err) {
-    console.error('Failed to copy: ', err);
-  }
+document.getElementById("contact").onclick = function() {
+    var text = document.getElementById("email").value;
+ 
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        console.log('Text copied to clipboard');
+    })
+    .catch(err => {
+        console.error('Error in copying text: ', err);
+    });
 }
